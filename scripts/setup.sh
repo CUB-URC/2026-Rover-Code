cbb9e2e5e696a1dd49ebe605be05b2fa3a120e3e
Avery Wagner <your.email@example.com>
2026-02-12 20:13:04 -0700

    add pose estimation stuff ; still need to test

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,3 +3,4 @@
 echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
 echo "source $(pwd)/ros2_ws/install/setup.bash" >> ~/.bashrc
 echo "export ROS_DOMAIN_ID=1" >> ~/.bashrc 
+echo "setup done. run 'source ~/.bashrc' to apply changes"
