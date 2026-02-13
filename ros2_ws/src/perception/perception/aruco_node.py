c4f48c174e3e7ea865bc14351781f1b813526fd7
Avery Wagner <your.email@example.com>
2026-02-12 20:14:06 -0700

    wtf that didnt work

--- ros2_ws/src/perception/perception/aruco_node.py
+++ ros2_ws/src/perception/perception/aruco_node.py
@@ -55,11 +55,13 @@ terminal 3: visualization
 
 """
 
+import yaml
 import rclpy
 from rclpy.node import Node
 from rclpy.qos import QoSProfile, ReliabilityPolicy, HistoryPolicy
 from sensor_msgs.msg import Image, CameraInfo
-from geometry_msgs.msg import PoseArray
+from geometry_msgs.msg import PoseArray, Pose, PoseStamped
+from scipy.spatial.transform import Rotation as R
 from cv_bridge import CvBridge
 
 import cv2
@@ -70,57 +72,20 @@ class ArucoNode(Node):
     def __init__(self):
         super().__init__("aruco_node")
 
-        # --- PARAMETERS ---
-        self.declare_parameter("aruco_dictionary_id", "DICT_4X4_50")
-        self.dictionary_id_name = (
-            self.get_parameter("aruco_dictionary_id").get_parameter_value().string_value
-        )
+        self._setup_parameters()
+        self._load_config()
+        self._setup_subscriptions()
+        self._setup_publishers()
 
-        # --- QOS PROFILE ---
-        # ZED uses "Best Effort" (UDP ish) transmission. we must match it
-        qos_profile = QoSProfile(
-            reliability=ReliabilityPolicy.BEST_EFFORT,
-            history=HistoryPolicy.KEEP_LAST,
-            depth=10,
-        )
+        # lookup tables 
+        self.marker_sizes = {}
+        self.marker_missions = {}
 
-        """ TODO
-        [] from camera calibration, get the intrinsic matrix and distortion coefficients
-        """
-
-        # --- PARAMETERS ---
-        self.declare_parameter("intrinsic_matrix", [])  # intrinsic matrix
-        self.declare_parameter("distortion_coeffs", [])  # distortion coefficients
-        self.declare_parameter(
-            "marker_length", 0.02
-        )  # default to 2 cm (adjust as needed) -> for pose estimation
-
+       # --- STATE INIT ---
         self.intrinsic_matrix = None
         self.distortion_coeffs = None
-
-        # --- SUBSCRIBERS ---
-        self.subscription = self.create_subscription(
-            Image,
-            "/image",  # remap ts
-            self.listener_callback,
-            qos_profile=qos_profile,  # qos profile Duh
-        )
-
-        # sub to camera info to get calibration data for pose estimation
-        self.camera_info_sub = self.create_subscription(
-            CameraInfo,  # todo
-            "/zed/zed_node/rgb/camera_info",
-            self.camera_info_callback,
-            qos_profile=qos_profile,
-        )
-
-        # --- PUBLISHERS ---
-        self.publisher_ = self.create_publisher(Image, "perception/aruco_debug", 10)
-
-        self.pose_publisher = self.create_publisher(
-            PoseArray, "perception/aruco_poses", 10  # todo  # ?
-        )
-
+        # todo: latest depth
+       
         # --- SETUP ---
         self.bridge = CvBridge()
 
@@ -136,6 +101,72 @@ class ArucoNode(Node):
                 f"Invalid ArUco Dictionary: {self.dictionary_id_name}"
             )
 
+    def _setup_parameters(self):
+        # --- PARAMETERS ---
+        config_path = self.declare_parameter("config_path", "config/aruco_config.yaml").get_parameter_value().string_value
+        with open(config_path, 'r') as f:
+            config = yaml.safe_load(f)
+
+        # self.declare_parameter("aruco_dictionary_id", "DICT_4X4_50")
+        
+        self.declare_parameter("aruco_dictionary_id", config.get('aruco_dictionary_id', 'DICT_4X4_50'))
+        self.dictionary_id_name = (
+            self.get_parameter("aruco_dictionary_id").get_parameter_value().string_value
+        )
+
+
+        self.declare_parameter("intrinsic_matrix", [])  # intrinsic matrix
+        self.declare_parameter("distortion_coeffs", [])  # distortion coefficients
+        self.declare_parameter(
+            "marker_length", 0.02
+        )  # default to 2 cm (adjust as needed) -> for pose estimation
+
+    def _setup_subscriptions(self):
+
+          # --- QOS PROFILE ---
+        # ZED uses "Best Effort" (UDP ish) transmission. we must match it
+        qos = QoSProfile(
+            reliability=ReliabilityPolicy.BEST_EFFORT,
+            history=HistoryPolicy.KEEP_LAST,
+            depth=10,
+        )
+
+        self.subscription = self.create_subscription(
+            Image,
+            "/image",  # remap ts
+            self.listener_callback,
+            qos_profile=qos
+        )
+
+        # sub to camera info to get calibration data for pose estimation
+        self.camera_info_sub = self.create_subscription(
+            CameraInfo,  # todo
+            "/zed/zed_node/rgb/camera_info",
+            self.camera_info_callback,
+            qos_profile=qos,
+        )
+
+    def _setup_publishers(self):
+        
+        self.publisher_ = self.create_publisher(Image, "perception/aruco_debug", 10)
+
+        self.pose_publisher = self.create_publisher(
+            PoseArray, "perception/aruco_poses", 10  # todo  # ?
+        )
+
+        # todo: per mission pubs (keyboard, usb, nav posts)
+        # todo: TF broadcaster for rviz
+
+    def _load_config(self):
+        """maps marker ids -> mission objectives"""
+
+        for mission, details in self.config['marker_mappings'].items():
+            for id in details['ids']:
+                self.marker_sizes[id] = details['size']
+                self.marker_missions[id] = mission
+
+        self.get_logger().info(f"Loaded config: {self.marker_missions}")
+
     def camera_info_callback(self, msg):
         """Get camera calibration from ZED camera_info topic for pose estimation"""
         if self.intrinsic_matrix is None:
@@ -149,7 +180,7 @@ class ArucoNode(Node):
             cv_image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
 
             # 2. detect markers
-            corners, ids, rejected = cv2.aruco.detectMarkers(
+            corners, ids, _ = cv2.aruco.detectMarkers(
                 cv_image, self.aruco_dict, parameters=self.aruco_params
             )
 
@@ -161,6 +192,53 @@ class ArucoNode(Node):
 
                 # 3.1 estimate pose
                 pose_array = PoseArray()
+                pose_array.header.stamp = msg.header.stamp
+                pose_array.header.frame_id = "zed_camera_frame" # ? adjust
+
+                for i, corner in enumerate(corners):
+                    marker_id = ids[i][0]
+                    marker_size = self.marker_sizes.get(marker_id, default=0.02) 
+
+
+                    # 3.2 estimate pose of each marker
+
+                    rvec, tvec, _ = cv2.aruco.estimatePoseSingleMarkers(
+                        corner,
+                        marker_size,
+                        self.intrinsic_matrix,
+                        self.distortion_coeffs,
+                    )
+
+                    # todo: integrate depth ?? or maybe not
+
+                    # 3.3 draw axis on image
+                    cv2.drawFrameAxes(cv_image, self.intrinsic_matrix, self.distortion_coeffs, rvec, tvec, marker_size * 0.5)
+
+                    # 3.4 conver to ROS pose message
+                    pose = Pose()
+                    pose.position.x = tvec[0][0][0]
+                    pose.position.y = tvec[0][0][1]
+                    pose.position.z = tvec[0][0][2]
+
+                    # 3.5 convert rotation matrix to quaternion 
+                    rotation_matrix = cv2.Rodrigues(rvec[0][0])[0]
+                    quat = R.from_matrix(rotation_matrix).as_quat()  # [x, y, z, w]
+                    pose.orientation.x = quat[0]
+                    pose.orientation.y = quat[1]
+                    pose.orientation.z = quat[2]
+                    pose.orientation.w = quat[3]
+
+                    pose_array.poses.append(pose)
+
+                    self.get_logger().info(
+                        f"Marker {marker_id}: pose=({tvec[0][0][0]:.3f}, )"
+                        f"({tvec[0][0][1]:.3f}, {tvec[0][0][2]:.3f}), "
+                    )
+
+                # 3.6 publish pose array
+                self.pose_publisher.publish(pose_array)
+                # todo: pub mission specific topics (?)
+
 
             # 4. publish debug image
             debug_msg = self.bridge.cv2_to_imgmsg(cv_image, "bgr8")
@@ -168,6 +246,35 @@ class ArucoNode(Node):
 
         except Exception as e:
             self.get_logger().error(f"Error processing image: {str(e)}")
+
+    def _publish_mission_objective(self, marker_id, pose, timestamp):
+        """Given a detected marker ID, publish the corresponding mission objective"""
+        mission = self.marker_missions.get(marker_id, "unknown")
+
+        pose_stamped = PoseStamped()
+        pose_stamped.header.stamp = timestamp
+        pose_stamped.header.frame_id = "zed_camera_frame"  # adjust as needed
+        pose_stamped.pose = pose
+
+        pub_map = {
+            "keyboard": self.keyboard_pub,
+            "usb": self.usb_pub,
+            "nav_post_1": self.nav_post_1_pub,
+            "nav_post_2": self.nav_post_2_pub,
+        }
+
+        pub = pub_map.get(mission)
+        if pub is not None:
+            pub.publish(pose_stamped)
+        else: 
+            self.get_logger().warn(f"No publisher found for mission: {mission}")
+
+        self.get_logger().info(f"Marker {marker_id} corresponds to mission: {mission}")
+
+    def get_marker_size(self, marker_id):
+        """Given mission context return marker size (m)"""
+
+    # todo: depth refine callback and depth callback (???)
 
 
 def main(args=None):
